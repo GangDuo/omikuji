972173e682a4024fbfcac8e922de4ab2d48b7cd5
Onoda Yoshiaki <y-onoda@nikki-grp.co.jp>
2021-12-13 12:57:11 +0900

    設定値から抽選を行う

--- src/Components/Omikuji.tsx
+++ src/Components/Omikuji.tsx
@@ -2,9 +2,29 @@ import FortuneConfig from './FortuneConfig';
 
 export default class Omikuji {
     constructor(private config: FortuneConfig[]) {
+        if(config.length === 0) {
+            throw new Error();
+        }
     }
 
     execute(): FortuneConfig {
-        return this.config[0];
+        const config = this.config;
+        const min = 1; // 最小値
+        const max = 1000; // 最大値
+    
+        // 乱数生成（抽選）
+        const randomNum = Math.floor(Math.random() * (max + 1 - min)) + min;
+    
+        let result = config[config.length - 1];
+        let total = 0;
+        for (let i = 0; i < config.length; i++) {
+            total += config[i].val;
+            if( randomNum <= total ) {
+                result = config[i];
+                break;
+            }
+        };
+    
+        return result;
     }
 }
